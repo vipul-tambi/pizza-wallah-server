f0c6f7acfc59be17b31795a65ec4b178e6bab7e6
Vipul Tambi <vt143831@gmail.com>
2023-02-09 18:49:19 +0530

    updated

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use("/api/users", require("./routes/userRoute"));
 app.use("/api/orders", require("./routes/orderRoute"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+  app.use("/", express.static("../client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
