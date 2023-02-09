14f848ebb2fd0eea1230e1e76e4fa5c02ceddc6c
Vipul Tambi <vt143831@gmail.com>
2023-02-09 17:25:18 +0530

    updated

--- config/config.js
+++ config/config.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 require("colors");
-
+const dotenv = require("dotenv");
+dotenv.config();
 const connectDB = async () => {
   try {
     const url = process.env.MONGO_URI;
     mongoose.set("strictQuery", false);
     const conn = await mongoose.connect(url, {
-      useUnifiedTopology: true,
       useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log("MongoDb connected");
   } catch (error) {
